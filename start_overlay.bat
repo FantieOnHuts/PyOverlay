83642823d0c72878fb2ccbd4f1a94b7d24edc7fb
FantieOnHuts <troytje14@gmail.com>
2025-05-22 17:34:42 +0200

    Starting bat first version
    A simple bat file used to install everything and start the overlay

---
+++ start_overlay.bat
@@ -0,0 +1,56 @@
+@echo off
+setlocal
+
+set VENV_DIR=.venv
+set SCRIPT_NAME=overlay_app.py
+set REQUIREMENTS=PyQt5 PyQtWebEngine keyboard
+
+REM check for python.
+python --version >nul 2>&1
+if %errorlevel% neq 0 (
+    No python.
+    exit /b 1
+)
+
+REM create virtual room if there is none.
+if not exist "%VENV_DIR%\Scripts\activate.bat" (
+    echo no virtual room "%VENV_DIR%".  Creating one.
+    python -m venv "%VENV_DIR%"
+    if %errorlevel% neq 0 (
+        echo Error creating virtual room.
+        pause
+        exit /b 1
+    )
+    echo virtual room created.
+)
+
+REM get into virtual.
+echo Activate virtual room...
+call "%VENV_DIR%\Scripts\activate.bat"
+if %errorlevel% neq 0 (
+    echo error activating virtual room.
+    pause
+    exit /b 1
+)
+
+REM  update dependencies.
+echo Installeren/updaten van dependencies: %REQUIREMENTS%
+pip install --upgrade pip >nul
+pip install %REQUIREMENTS%
+if %errorlevel% neq 0 (
+    echo error installing dependecies.
+    pause
+    exit /b 1
+)
+
+REM start.
+echo Start %SCRIPT_NAME%...
+echo F9 to toggle see trough.
+echo F8 to click trough mode.
+
+python "%SCRIPT_NAME%"
+
+echo closed.
+deactivate
+endlocal
+pause
